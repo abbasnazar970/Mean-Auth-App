5af7c6c2a8f21f98f61f345c6bf938d636847b56
Abbas Nazar <abbasnazar.970@gmail.com>
2017-11-10 14:38:36 +0500

    UserModel&Register

---
+++ models/users.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const bycrypt = require('bcryptjs');
+const config = require("../config/db");
+
+//schema
+
+    const UserSchema = mongoose.Schema({
+       name:{
+           type:String
+       },
+        email:
+            {
+                type:String,
+                required:true
+            },
+        username:
+            {
+               type:String,
+                required:true
+            },
+            password :{
+            type: String,
+                required:true
+            }
+
+
+    });
+
+    const User= module.exports = mongoose.model("User",UserSchema);
+
+    module.exports.getUserID=(id,callback)=>{
+    User.findByid(id,callback);
+}
+
+module.exports.getUserByUsername=(username,callback)=>{
+        const query={username:username};
+        User.findOne(query,callback);
+}
+
+module.exports.addUser = function (newUser, callback) {
+    bycrypt.genSalt(10,(err,salt)=>{
+        bycrypt.hash(newUser.password,salt,(err,hash)=>{
+        if(err) throw err;
+            newUser.password=hash;
+            newUser.save(callback);
+    });
+    });
+}
+
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 const express=require("express");
 const router=express.Router();
+const passport=require("passport");
+const jwt=require("jsonwebtoken");
+const User=require("../models/users");
 
 
-router.get("/register",(req,res,next)=>{
-    res.send("register");
+router.post("/register",(req,res,next)=>{
+
+    let newUser= new User({
+        name:req.body.name,
+        email:req.body.email,
+        username:req.body.username,
+        password:req.body.password
+    });
+
+    User.addUser(newUser,(err,user)=>{
+            if(err){
+                res.json({sucess:false,msg:'failed to register user'});
+            }else
+            {
+                res.json({sucess:true,msg:'user registered'});
+            }
+    });
 });
 
 router.get("/authenticate",(req,res,next)=>{
@@ -14,9 +32,6 @@ router.get("/profile",(req,res,next)=>{
     res.send("profile");
 });
 
-router.get("/validate",(req,res,next)=>{
-    res.send("validate");
-});
 
 
 module.exports=router;
